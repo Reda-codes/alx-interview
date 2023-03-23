2d982f13c081c77ee2f0f2edf442a7044dc72293
Reda <muhammadredabh@gmail.com>
2023-03-23 11:10:00 +0000

    0. Pascals Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import math
+"""
+pascal_triangle(n) a function returns a list of lists
+of integers representing the Pascal triangle of n
+"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    else:
+        ls = []
+        for i in range(n):
+            nums = []
+            for j in range(i+1):
+                nums.append(math.comb(int(i), int(j)))
+            ls.append(nums)
+        return ls
