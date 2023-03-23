c3d5afa05c7e5cd86b0139ff0a0546bee7d1f278
Reda <muhammadredabh@gmail.com>
2023-03-23 13:19:24 +0000

    new tests

--- 0x00-pascal_triangle/0-main.py
+++ 0x00-pascal_triangle/0-main.py
@@ -16,3 +16,6 @@ def print_triangle(triangle):
 if __name__ == "__main__":
     print_triangle(pascal_triangle(5))
     print_triangle(pascal_triangle(11))
+    print_triangle(pascal_triangle(0))
+    print_triangle(pascal_triangle(1))
+    print_triangle(pascal_triangle(-11))
