105169c909761b90e9c11db50715ae4b407142db
Reda <muhammadredabh@gmail.com>
2023-03-23 12:50:00 +0000

    Removed math.comb and replaced it with my own function

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,6 +5,18 @@ representing the Pascal’s triangle of n
 '''
 
 
+def combi(n1, n2):
+    '''function similar to math.comb()'''
+    num1 = n1
+    num2 = n2
+    for i in range(1, n2):
+        num1 = num1 * (n1 - i)
+        num2 = num2 * (n2 - i)
+    if num2 == 0:
+        return(1)
+    return(int(num1/num2))
+
+
 def pascal_triangle(n):
     ''' pascal_triangle(n) function '''
     if n <= 0:
@@ -13,7 +25,7 @@ def pascal_triangle(n):
         ls = []
         for i in range(n):
             nums = []
-            for j in range(i+1):
-                nums.append(math.comb(int(i), int(j)))
+            for j in range(0, i+1):
+                nums.append(combi(i, j))
             ls.append(nums)
         return ls
