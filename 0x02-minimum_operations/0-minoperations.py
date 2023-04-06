40cd544a1155a3f229520bb021c46ab5d9f317c6
Reda <muhammadredabh@gmail.com>
2023-04-06 17:06:52 +0000

    0. Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+minOperations which calculates the fewest number of operations
+needed to result in exactly n H characters in the file
+'''
+import math
+
+
+def minOperations(n):
+    if n == 1:
+        return 0
+
+    return 0
