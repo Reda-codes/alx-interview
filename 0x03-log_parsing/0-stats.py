80069fad813f884f8bc8e3b83582ad86dc74a9b8
Reda <muhammadredabh@gmail.com>
2023-04-13 19:28:38 +0000

    Post QA Check

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,25 +2,34 @@
 ''' script that reads stdin line by line and computes metrics '''
 import sys
 
-
 if __name__ == "__main__":
     count = 0
     totalSize = 0
-    statusList = []
+    codes = [200, 301, 400, 401, 403, 404, 405, 500]
+    obj = {}
     for line in sys.stdin:
         lst = line.split(' ')
-        status = int(lst[-2])
-        size = int(lst[-1])
-        if count == 10:
-            print('File size: {}'.format(totalSize))
-            count = 0
-            statusList.sort()
-            printed = []
-            for el in statusList:
-                if el not in printed:
-                    print('{}: {}'.format(el, statusList.count(el)))
-                    printed.append(el)
-            statusList = []
-        count += 1
-        totalSize += size
-        statusList.append(status)
+        if len(lst) < 2:
+            continue
+        try:
+            count += 1
+            status = int(lst[-2])
+            size = int(lst[-1])
+            totalSize += size
+            if status in codes:
+                if status in obj:
+                    obj[status] += 1
+                else:
+                    obj[status] = 1
+
+            if count == 10:
+                count = 0
+                print('File size: {}'.format(totalSize))
+                for el in sorted(obj.items()):
+                    print('{}: {}'.format(el[0], el[1]))
+        except ValueError:
+            continue
+
+    print('File size: {}'.format(totalSize))
+    for el in sorted(obj.items()):
+        print('{}: {}'.format(el[0], el[1]))
