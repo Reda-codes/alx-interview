0605a26bc5f6bc59b8a7687f5b8d440fc8314b2b
Reda <muhammadredabh@gmail.com>
2023-04-13 14:46:05 +0000

    fixed script for checker

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,27 +3,28 @@
 import sys
 
 
-count = 0
-totalSize = 0
-statusList = []
+if __name__ == "__main__":
+    count = 0
+    totalSize = 0
+    statusList = []
 
-for line in sys.stdin:
-    lst = line.split(' ')
-    status = int(lst[-2])
-    size = int(lst[-1])
+    for line in sys.stdin:
+        lst = line.split(' ')
+        status = int(lst[-2])
+        size = int(lst[-1])
 
-    if count == 10:
-        print(f'File size:{totalSize}')
-        count = 0
-        statusList.sort()
-        printed = []
+        if count == 10:
+            print(f'File size:{totalSize}')
+            count = 0
+            statusList.sort()
+            printed = []
 
-        for el in statusList:
-            if el not in printed:
-                print(f'{el}: {statusList.count(el)}')
-                printed.append(el)
-        statusList = []
+            for el in statusList:
+                if el not in printed:
+                    print(f'{el}: {statusList.count(el)}')
+                    printed.append(el)
+            statusList = []
 
-    count += 1
-    totalSize += size
-    statusList.append(status)
+        count += 1
+        totalSize += size
+        statusList.append(status)
