725c85f6237f9856c850bd807035edf6aa60c74c
Reda <muhammadredabh@gmail.com>
2023-04-13 14:39:18 +0000

    0. Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,5 +3,27 @@
 import sys
 
 
+count = 0
+totalSize = 0
+statusList = []
+
 for line in sys.stdin:
-    print(f'{line}')
+    lst = line.split(' ')
+    status = int(lst[-2])
+    size = int(lst[-1])
+
+    if count == 10:
+        print(f'File size:{totalSize}')
+        count = 0
+        statusList.sort()
+        printed = []
+
+        for el in statusList:
+            if el not in printed:
+                print(f'{el}: {statusList.count(el)}')
+                printed.append(el)
+        statusList = []
+
+    count += 1
+    totalSize += size
+    statusList.append(status)
