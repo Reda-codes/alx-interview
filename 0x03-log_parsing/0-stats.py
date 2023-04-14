f71e17793ae7ce2d5691f4da59594eb956e9d1e1
Reda <muhammadredabh@gmail.com>
2023-04-14 13:29:11 +0000

    keyboard interruption

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,29 +7,36 @@ if __name__ == "__main__":
     totalSize = 0
     codes = [200, 301, 400, 401, 403, 404, 405, 500]
     obj = {}
-    for line in sys.stdin:
-        lst = line.split(' ')
-        if len(lst) < 2:
-            continue
-        try:
-            count += 1
-            status = int(lst[-2])
-            size = int(lst[-1])
-            totalSize += size
-            if status in codes:
-                if status in obj:
-                    obj[status] += 1
-                else:
-                    obj[status] = 1
+    try:
+        for line in sys.stdin:
+            lst = line.split(' ')
+            if len(lst) < 2:
+                continue
+            try:
+                count += 1
+                status = int(lst[-2])
+                size = int(lst[-1])
+                totalSize += size
+                if status in codes:
+                    if status in obj:
+                        obj[status] += 1
+                    else:
+                        obj[status] = 1
 
-            if count == 10:
-                count = 0
-                print('File size: {}'.format(totalSize))
-                for el in sorted(obj.items()):
-                    print('{}: {}'.format(el[0], el[1]))
-        except ValueError:
-            continue
+                if count == 10:
+                    count = 0
+                    print('File size: {}'.format(totalSize))
+                    for el in sorted(obj.items()):
+                        print('{}: {}'.format(el[0], el[1]))
 
-    print('File size: {}'.format(totalSize))
-    for el in sorted(obj.items()):
-        print('{}: {}'.format(el[0], el[1]))
+            except ValueError:
+                continue
+
+        print('File size: {}'.format(totalSize))
+        for el in sorted(obj.items()):
+            print('{}: {}'.format(el[0], el[1]))
+
+    except KeyboardInterrupt:
+        print('File size: {}'.format(totalSize))
+        for el in sorted(obj.items()):
+            print('{}: {}'.format(el[0], el[1]))
