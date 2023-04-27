9053c946c8d1094b91e9a553f3c972d39e5b0ec1
Reda <muhammadredabh@gmail.com>
2023-04-27 17:53:42 +0000

    extra cases & pep8 fix

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -5,6 +5,13 @@ import sys
 
 def nqueens(N):
 
+    if not isinstance(N, int):
+        print("N must be a number")
+        sys.exit(1)
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
     def queenLoc(board, row, col):
         for r, c in board:
             if r == row or c == col or r+c == row+col or r-c == row-col:
@@ -24,5 +31,14 @@ def nqueens(N):
 
 
 if __name__ == '__main__':
-    
-    nqueens(int(sys.argv[1]))
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    nqueens(N)
