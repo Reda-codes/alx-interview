f525e1fd548e600b61f1ab8a45b2c7e808926eaf
Reda <muhammadredabh@gmail.com>
2023-04-27 17:12:01 +0000

    0. N queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,4 +1,28 @@
 #!/usr/bin/python3
 """ The N queens puzzle """
+import sys
 
 
+def nqueens(N):
+
+    def queenLoc(board, row, col):
+        for r, c in board:
+            if r == row or c == col or r+c == row+col or r-c == row-col:
+                return False
+        return True
+
+    def solve(board, row):
+        if row == N:
+            print(board)
+            return None
+
+        for col in range(N):
+            if queenLoc(board, row, col):
+                solve(board + [[row, col]], row+1)
+
+    solve([], 0)
+
+
+if __name__ == '__main__':
+    
+    nqueens(int(sys.argv[1]))
