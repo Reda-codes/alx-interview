387142008d6df321b1601de893b96aa8917a8fa8
Reda <muhammadredabh@gmail.com>
2023-05-25 10:21:02 +0100

    Removed import

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 ''' 0-island_perimeter file '''
-from typing import List
 
 
-def island_perimeter(grid: List[List[int]]) -> int:
+def island_perimeter(grid):
     ''' island_perimeter function '''
     perimeter = 0
     for i in range(len(grid)):
